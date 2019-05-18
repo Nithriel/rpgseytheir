c980273cb60b9da0535aa98c79b39f4597e8b6ba
Raphael Pletz <raphael.pletz00@gmail.com>
2019-05-18 15:25:58 -0700

    editable character

--- forum.js
+++ forum.js
@@ -12,6 +12,7 @@ router.post('/:genre/add_character', add_character);
 router.post('/add_reply', add_reply);
 router.post('/delete_post', delete_post);
 router.post('/edit_post', edit_post);
+router.post('/edit_character', edit_character);
 router.get('/clearnotification', clearNotification);
 
 function get_date() {
@@ -94,6 +95,48 @@ function add_character(request, response) {
             response.send('Unable to post message');
         }
         response.redirect('/genre_board/' + chosen_genre);
+    });
+}
+
+function edit_character(request, response) {
+    var ObjectId = utils.getObjectId();
+
+    var buffs = [request.body.buff1, request.body.buff2, request.body.buff3, request.body.buff4 ,request.body.buff5];
+    var debuffs = [request.body.debuff1, request.body.debuff2, request.body.debuff3, request.body.debuff4, request.body.debuff5];
+    var status = request.body.char_status;
+    var inventory = request.body.inventory;
+    var skills = request.body.skills;
+    var personal_sheet = request.body.personal_sheet;
+    var look = request.body.look;
+    var personality = request.body.personality;
+    var personal_personality = request.body.personality;
+    var story = request.body.story;
+    var personal_story = request.body.personal_story;
+    var id = ObjectId(request.body.id);
+
+    var db = utils.getDb();
+
+    db.collection('messages').findOneAndUpdate({
+        _id: id
+    }, {
+        $set: {
+            buffs: buffs,
+            debuffs: debuffs,
+            status: status,
+            inventory: inventory,
+            skills: skills,
+            personal_sheet: personal_sheet,
+            look: look,
+            personality: personality,
+            personal_personality: personal_personality,
+            story: story,
+            personal_story: personal_story,
+        }
+    }, (err, items) => {
+        if (err) {
+            response.send('error in updating the character')
+        }
+        response.redirect('back')
     });
 }
 
